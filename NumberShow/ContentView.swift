abd592b7ea1f9032bda1357c951dab5d952f7d2c
leskerito <franck.kindia@outlook.fr>
2023-11-15 16:55:41 +0100

    Initial Commit

---
+++ NumberShow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NumberShow
+//
+//  Created by Franck Kindia on 15/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NumberShow/NumberShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberShowApp.swift
+//  NumberShow
+//
+//  Created by Franck Kindia on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct NumberShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
